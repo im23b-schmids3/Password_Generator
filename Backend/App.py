90aeb901822459bf9a2998724db205c7ca7517f3
schmids3 <schmids3@bzz.ch>
2024-12-28 21:52:08 +0100

    add comments

--- Backend/App.py
+++ Backend/App.py
@@ -2,16 +2,18 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 from Main import generate_password
 
+# Initialize the Flask app and enable CORS for cross-origin requests
 app = Flask(__name__)
 CORS(app)
 
 
 @app.route("/submit", methods=["POST"])
 def get_input():
-    data = request.json.get("data")
-    amount_characters = int(data)
-    password = generate_password(amount_characters)
-    return jsonify({"message": f"Your generated password: {password}"})
+    data = request.json.get("data")  # Extract the 'data' field from the request body
+    amount_characters = int(data)  # Convert the input to an integer (number of characters)
+    password = generate_password(amount_characters)  # Generate a password using the imported function
+    return jsonify(
+        {"message": f"Your generated password: {password}"})  # Return the generated password in a JSON response
 
 
 if __name__ == "__main__":
--- Backend/Main.py
+++ Backend/Main.py
@@ -2,6 +2,7 @@ import string
 import random
 
 
+# Generates a random password
 def generate_password(amount_characters):
     characters = string.ascii_letters + string.digits + string.punctuation
     password = ''.join(random.choice(characters) for _ in range(amount_characters))
